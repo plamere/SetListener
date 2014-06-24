b3a1521edf7d9bab8e337d8f7bc47638170f1d92
Paul Lamere <Paul.Lamere@gmail.com>
2014-06-24 13:18:39 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,22 @@
+# The Set Listener
+
+This is the source for a web app called The Set Listener. The app
+creates a Spotify playlist for your favorite artist's most recent
+show. The app uses the Setlist.fm API and the Spotify API.
+
+The app is online at [The Set Listener](http://static.echonest.com/SetListner)
+
+<img src="http://static.echonest.com/SetListener/ss.png" width=600>
+
+
+# The Server
+
+The Set Listener has a server component that manages the API interaction with setlist.fm
+because the setlist.fm API doesn't support cross-domain access.
+
+
+# The Web App
+The web app is a relatively simple app that solicits and artist name from the user, finds
+recents shows by that artist via setlist.fm, looks up the Spotify tracks via the Spotify Web
+API, and saves them to the user's Spotify Playlist after authorizing the user.
+
---
+++ server/server.py
@@ -0,0 +1,55 @@
+import os 
+import sys
+import cherrypy
+import ConfigParser
+import urllib2
+import json
+import webtools
+import requests
+
+
+class SetlistServer(object):
+    def __init__(self, config):
+        self.production_mode = config.getboolean('settings', 'production')
+
+
+    @cherrypy.tools.caching(delay=3600)
+    def query(self, q):
+        cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
+        cherrypy.response.headers['Content-Type']= 'application/json'
+
+        if 'setlist.fm' in q:
+            r = requests.get(q)
+            print "calling setlist.fm"
+            return r.text.encode('utf8')
+        else:
+            return '{}'
+    query.exposed = True
+
+if __name__ == '__main__':
+    urllib2.install_opener(urllib2.build_opener())
+    conf_path = os.path.abspath('web.conf')
+    print 'reading config from', conf_path
+    cherrypy.config.update(conf_path)
+
+    config = ConfigParser.ConfigParser()
+    config.read(conf_path)
+    production_mode = config.getboolean('settings', 'production')
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # Set up site-wide config first so we get a log if errors occur.
+
+    if production_mode:
+        print "Starting in production mode"
+        cherrypy.config.update({'environment': 'production',
+                                'log.error_file': 'simdemo.log',
+                                'log.screen': True})
+    else:
+        print "Starting in development mode"
+        cherrypy.config.update({'noenvironment': 'production',
+                                'log.error_file': 'site.log',
+                                'log.screen': True})
+
+    conf = webtools.get_export_map_for_directory("static")
+    cherrypy.quickstart(SetlistServer(config), '/SetlistServer', config=conf)
+
